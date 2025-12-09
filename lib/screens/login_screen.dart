a662b8ec83a08d549c36c7b187caf1c635a39173
FadillPro <carleskar276@gmail.com>
2025-12-09 17:34:16 +0700

    Login_screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../providers/auth_provider.dart';
+
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login EcoPatrol')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const TextField(decoration: InputDecoration(labelText: 'Username')),
+              const TextField(decoration: InputDecoration(labelText: 'Password', obscureText: true)),
+              const SizedBox(height: 20),
+              // Tombol Login: Memanggil method login dari AuthNotifier
+              ElevatedButton(
+                onPressed: () {
+                  // Panggil .notifier untuk mengubah state
+                  ref.read(authProvider.notifier).login();
+                },
+                child: const Text('Login'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
